0a4dd8615ac3821319f5a6db8d0f04db45a9c5ee
Chidi <57526824+datalifenyc@users.noreply.github.com>
2022-03-13 10:28:50 -0400

    Binder build successful with direct link; maps notebook cleanup

--- teaapp/app.py
+++ teaapp/app.py
@@ -21,20 +21,51 @@ st.write(
     """
     # Bubble Tea Shop App
 
-    ## About
-
     Analyzing Bubble Tea Shops in NYC
-
-    * **Data Team**: Mark Bauer, Chidi Ezeolu, Ho Hsieh, Nathan Williamson
-    * **GitHub**: https://github.com/mebauer/boba-nyc
-    * **Jupyter Book**: https://boba-nyc.datalife.nyc/
-    * **Python Libraries**: [pandas](https://pandas.pydata.org/) | [streamlit](https://streamlit.io/) | [os](https://docs.python.org/3/library/os.html) | [pillow](https://pillow.readthedocs.io/en/stable/index.html)
-    * **Data Sources**: [Yelp Fusion API - Business Search](https://www.yelp.com/developers/documentation/v3/business_search)
-    * **Title Image**: [VIVI BUBBLE TEA - 49 Bayard St.](https://vivibubbletea.com/)
-    
-    ## References
-
-    * [Streamlit Gallery](https://streamlit.io/gallery)
-    * [Build 12 Data Science Apps with Python and Streamlit - Full Course | Author: Chanin Nantasenamat (dataprofessor) | Channel: freeCodeCamp.org](https://www.youtube.com/watch?v=JwSS70SZdyM)
     """
 )
+with st.expander('Show Info'):
+    st.write(
+        """
+        * **Data Team**: Mark Bauer, Chidi Ezeolu, Ho Hsieh, Nathan Williamson
+        * **GitHub**: https://github.com/mebauer/boba-nyc
+        * **Jupyter Book**: https://boba-nyc.datalife.nyc/
+        * **Python Libraries**: [pandas](https://pandas.pydata.org/) | [streamlit](https://streamlit.io/) | [os](https://docs.python.org/3/library/os.html) | [pillow](https://pillow.readthedocs.io/en/stable/index.html)
+        * **Data Sources**: [Yelp Fusion API - Business Search](https://www.yelp.com/developers/documentation/v3/business_search)
+        * **Title Image**: [VIVI BUBBLE TEA - 49 Bayard St.](https://vivibubbletea.com/)
+        
+        ## References
+
+        * [Streamlit Gallery](https://streamlit.io/gallery)
+        * [Build 12 Data Science Apps with Python and Streamlit - Full Course | Author: Chanin Nantasenamat (dataprofessor) | Channel: freeCodeCamp.org](https://www.youtube.com/watch?v=JwSS70SZdyM)
+        """
+    )
+
+store_counts_df = pd.read_csv(file_path + '/name_counts.csv', index_col = [0])
+max_stores = int(store_counts_df['counts'].max())
+default_store_count = 4
+
+st.sidebar.header('Select Options')
+store_count = st.sidebar.slider('Select Min # of Stores', 1, max_stores, default_store_count)
+
+def view_stores_by_count(count_of_stores):
+    stores_df = store_counts_df[store_counts_df['counts'] >= store_count]
+    stores_df = stores_df.rename(columns = {
+        'name': 'Bubble Tea Shop Name',
+        'counts': '# of Stores in 5 Boros',
+    })
+    stores_df.index += 1
+    return stores_df
+
+# def user_options():
+#     store_count = st.sidebar.slider('# of Stores', 1, max_stores, default_store_count)
+#     data = {
+#         'store_count': store_count,
+#     }
+#     options = pd.DataFrame(data, index = [0])
+#     return options
+
+stores_counts_df = view_stores_by_count(store_count)
+
+st.subheader('Store Counts')
+st.write(stores_counts_df)
